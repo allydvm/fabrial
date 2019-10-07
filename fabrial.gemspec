2399b57a136f1ad4de56de265ea08e87a918d854
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2019-10-07 09:51:53 -0400

    add database cleaner

--- fabrial.gemspec
+++ fabrial.gemspec
@@ -51,4 +51,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'sqlite3', '~> 1.3.0'
   spec.add_development_dependency 'pry'
   spec.add_development_dependency 'pry-byebug'
+  spec.add_development_dependency 'database_cleaner'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,7 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/focus'
+require 'database_cleaner'
 
 if ENV['CONTINUOUS_INTEGRATION']
   Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(color: true)
@@ -19,3 +20,14 @@ end
 
 ActiveSupport::TestCase.extend(Minitest::Spec::DSL)
 ActiveSupport::TestCase.test_order = :alpha
+
+DatabaseCleaner.strategy = :transaction
+class ActiveSupport::TestCase
+  before do
+    DatabaseCleaner.start
+  end
+
+  after do
+    DatabaseCleaner.clean
+  end
+end
