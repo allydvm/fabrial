84bfb2b7e86acf5d6f5349f267cf850ce5b8ee7f
Tad Thorley <phaedryx@gmail.com>
2019-10-07 20:54:29 -0600

    callhash added

--- lib/fabrial/create.rb
+++ lib/fabrial/create.rb
@@ -3,28 +3,38 @@
 module Create
   attr_accessor :defaults
 
+  class CallHash < Hash
+    def [](key)
+      value = super
+      value.respond_to?(:call) ? value.call : value
+    end
+
+    def fetch(key)
+      value = super
+      value.respond_to?(:call) ? value.call : value
+    end
+  end
+
   def create(klass, data = {})
     # Find the base class of any STI types
     base = klass.base_class
 
-    default_data = defaults[klass]
-    default_data ||= defaults[base]
-    default_data ||= {}
+    default_data = CallHash.new.merge(defaults[klass])
+    default_data = default_data.merge(defaults[base])
+    default_data = default_data.merge(data)
 
-    data = default_data.merge data
-    klass.create! data
+    klass.create! default_data
   end
 
-  module AutoIds
-    extend self
-
-    @ids = {}
-
-    # Get the next auto-incrementing id for the given klass
-    def next(klass)
-      id = @ids[klass] || 1
-      @ids[klass] += 1
-      id
+  def serial_number(start=1, step=1)
+    serializer = Enumerator.new do |yielder|
+      loop do
+        yielder << start
+        start += step
+      end
     end
+
+    -> { serializer.next }
   end
+  alias :sn :serial_number
 end
